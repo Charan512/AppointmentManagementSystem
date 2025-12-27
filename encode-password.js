1a983f6dd47da08bdcd274ef2487d28d356587e8
sri ram charan <nvsk72@gmail.com>
2025-12-27 22:07:41 +0530

    Clean up: Remove test and temporary files

--- encode-password.js
+++
@@ -1,8 +0,0 @@
-// URL encode password for MongoDB connection string
-const password = 'cHaran@55';
-const encodedPassword = encodeURIComponent(password);
-
-console.log('Original password:', password);
-console.log('Encoded password:', encodedPassword);
-console.log('\nUse this in your MONGODB_URI:');
-console.log(`mongodb+srv://<username>:${encodedPassword}@cluster1.ymf1gqe.mongodb.net/appointment_queue_db?retryWrites=true&w=majority&appName=Cluster1`);
